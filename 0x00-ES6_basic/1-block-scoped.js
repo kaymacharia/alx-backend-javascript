add01b2f844970d6f68dbb20b08e500112b55259
kaymacharia <kaymachariaa@gmail.com>
2024-05-27 18:44:10 +0300

    Fueled by gallons of coffee, I wrangled these files into submission.

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // Add code inside the if statement if necessary
+  }
+
+  return [task, task2];
+}
